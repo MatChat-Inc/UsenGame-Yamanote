6a7389b3606cc64c06cd9eebe09dd20c7c2ddbd9
LunarEclipse <949886@qq.com>
2025-07-28 14:12:21 +0800

    Show version dynamically for app info view.

--- Assets/Game/Common/AppInfoView.cs
+++ Assets/Game/Common/AppInfoView.cs
@@ -3,11 +3,19 @@
 using Luna.UI;
 using Luna.UI.Navigation;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace USEN.Games.Common
 {
     public class AppInfoView : Widget
     {
+        public Text versionText;
+        
+        private void Start()
+        {
+            versionText.text = $"Version {Application.version}";
+        }
+        
         private void Update()
         {
             if (Input.GetKeyDown(KeyCode.Escape) ||
